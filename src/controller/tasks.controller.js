e32e6610ac4ec75c3ae7720d749a7317f22bc4bc
lukecode <lucassocorrosilva@gmail.com>
2022-11-09 10:16:58 -0300

    Função put criada

--- src/controller/tasks.controller.js
+++ src/controller/tasks.controller.js
@@ -1,26 +1,27 @@
 const pool = require("../db");
 
 const getAllTasks = async (req, res) => {
-    try {
-    const allTasks = await pool.query('SELECT * from task')
-    res.send(allTasks.rows)
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    const allTasks = await pool.query("SELECT * from task");
+    res.send(allTasks.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-const getTask = async(req, res) => {
- try {
-   const {id} = req.params
-  const result = await pool.query('SELECT * FROM task WHERE id = $1', [id]);
+const getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
 
-  if(result.rows.length === 0) return res.status(404).json({
-    message: "Tarefa não encontrada!"
-  })
-  res.json(result.rows[0]);
- } catch (error) {
-    console.log(error.message)
- }
+    if (result.rows.length === 0)
+      return res.status(404).json({
+        message: "Tarefa não encontrada!",
+      });
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const createTask = async (req, res) => {
@@ -28,30 +29,41 @@ const createTask = async (req, res) => {
 
   try {
     const result = await pool.query(
-    "INSERT INTO task (title,description) VALUES ($1, $2) RETURNING *",
-    [title, description]
-  );
-  res.send(result.rows[0]);
+      "INSERT INTO task (title,description) VALUES ($1, $2) RETURNING *",
+      [title, description]
+    );
+    res.send(result.rows[0]);
   } catch (error) {
-    res.json({error: error.message})
+    res.json({ error: error.message });
   }
-  
 };
 
-const deleteTask = async(req, res) => {
-  const { id } = req.params
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
   const result = await pool.query("DELETE FROM task WHERE id = $1", [id]);
 
-  if(result.rowCount.length === 0) 
-  return res.status(404).json({
-    message: "Tarefa deletada!"
-  });
+  if (result.rowCount.length === 0)
+    return res.status(404).json({
+      message: "Tarefa deletada!",
+    });
 
   return res.sendStatus(204);
 };
 
-const updateTask = (req, res) => {
-  res.send("retrieving a list of tasks");
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  const result = await pool.query(
+    "UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *", [title,description,id]
+  );
+  if(result.rows.length === 0){
+    return res.status(404).json({
+      message: "Tarefa não existe",
+    });
+  }
+  console.log(result)
+  return res.json(result.rows[0]);
 };
 
 module.exports = {
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -7,7 +7,7 @@ router.get("/tasks", getAllTasks);
 router.get("/tasks/:id", getTask);
 router.post("/tasks", createTask);
 router.delete("/tasks/:id", deleteTask);
-router.put("/tasks", updateTask);
+router.put("/tasks/:id", updateTask);
 
 
 module.exports = router;
